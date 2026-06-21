85038e29ead0a6089c177396a0d08aad305d5b2f
meellowamg-lang <meellowamg@gmail.com>
2026-06-21 10:50:21 +0200

    Initial commit

---
+++ src/main/java/net/meellowamg/bettermapsmod/BetterMapsMod.java
@@ -0,0 +1,20 @@
+package net.meellowamg.bettermapsmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BetterMapsMod implements ModInitializer {
+	public static final String MOD_ID = "bettermapsmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+	public static Identifier id(String path) {
+		return Identifier.fromNamespaceAndPath(MOD_ID, path);
+	}
+}
